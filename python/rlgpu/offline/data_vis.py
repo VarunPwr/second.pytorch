b803cc7499be59a4f7311fece91d3467c1e14e06
Mehooz <mehoozhang@gmail.com>
2021-08-14 02:04:20 -0700

    fix vis tool

--- python/rlgpu/offline/data_vis.py
+++ python/rlgpu/offline/data_vis.py
@@ -8,10 +8,14 @@ for k, v in data_dict.items():
         v = np.expand_dims(v, -1)
     elif len(np.shape(v)) == 3:
         v = v.reshape(v.shape[0], v.shape[1] * v.shape[2])
+    # v = v[:100]
     X_tsne = TSNE(n_components=2).fit_transform(v)
     x_min, x_max = X_tsne.min(0), X_tsne.max(0)
     X_norm = (X_tsne - x_min) / (x_max - x_min)
     plt.figure(figsize=(8, 8))
+    for i in range(X_norm.shape[0]):
+        plt.text(X_norm[i, 0], X_norm[i, 1], '.', 
+                fontdict={'weight': 'bold', 'size': 8})
     plt.xticks([])
     plt.yticks([])
     plt.savefig("vis/{}.png".format(k))
